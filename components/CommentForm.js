e02478e3f8efc8d88262e3bbf7227cd9406afeb0
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2023-02-20 16:52:02 +0900

    comment

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import useInput from '../hooks/useInput';
+import { Form, Button, Input } from 'antd';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
-const CommentForm = () => {
-  return <div>CommentForm</div>;
+const CommentForm = ({ post }) => {
+  const id = useSelector((state) => state.user.me?.id);
+  const [commentText, onChangeCommentText] = useInput('');
+
+  const onSubmitComment = useCallback(() => {
+    console.log(post.id, commentText);
+  }, [post, commentText]);
+
+  return (
+    <Form onFinish={onSubmitComment}>
+      <Form.Item style={{ position: 'relative', margin: 0 }}>
+        <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4} />
+        <Button type="primary" htmlType="submit" style={{ position: 'absolute', zIndex: 1, right: 0, bottom: -40 }}>
+          삐약
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+CommentForm.propTypes = {
+  post: PropTypes.object.isRequired,
 };
 
 export default CommentForm;
