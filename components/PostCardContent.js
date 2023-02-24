25fc9172924685fc32a480e933e64e8dd7e61a01
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2023-02-24 14:21:09 +0900

    feat: 해시태그 기능 추가

--- components/PostCardContent.js
+++ components/PostCardContent.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
 
-const PostCardContent = () => {
+const PostCardContent = ({ postData }) => {
+  // 첫 번째 게시글 #해시태그#익스프레스
   return (
-    <div>PostCardContent</div>
-  )
-}
+    <div>
+      {postData.split(/(#[^\s#]+)/g).map((v, i) => {
+        if (v.match(/(#[^\s#]+)/g)) {
+          return (
+            <Link href={`/hashtag/${v.slice(1)}`} key={i}>
+              <a>{v}</a>
+            </Link>
+          );
+        }
+        return v;
+      })}
+    </div>
+  );
+};
 
-export default PostCardContent
+PostCardContent.propTypes = { postData: PropTypes.string.isRequired };
+
+export default PostCardContent;
